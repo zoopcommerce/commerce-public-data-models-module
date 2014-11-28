f5b4dca24b7ee4429d5e875ba3a839e34a171e59
dollyaswin <dolly.aswin@gmail.com>
2014-11-29 01:01:31 +0700

    Add token for PayPal transaction

--- src/Zoop/Payment/Gateway/Paypal/Express/DataModel/Transaction.php
+++ src/Zoop/Payment/Gateway/Paypal/Express/DataModel/Transaction.php
@@ -15,4 +15,25 @@ use Zoop\Shard\Annotation\Annotations as Shard;
  */
 class Transaction extends AbstractTransaction
 {
+    /**
+     *
+     * @ODM\String
+     */
+    protected $token;
+    
+    /**
+     * @return string $token
+     */
+    public function getToken()
+    {
+        return $this->token;
+    }
+    
+    /**
+     * @param string $token
+     */
+    public function setToken($token)
+    {
+        $this->token = $token;
+    }
 }
