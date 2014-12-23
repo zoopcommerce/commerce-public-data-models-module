1f4f29f66b1f02e030958db0c5bd7efeeddfd869
crimsonronin <josh.stuart@zoopcommerce.com>
2014-12-23 19:58:22 +1100

    Changed to entities filter

--- src/Zoop/User/DataModel/AbstractUserFilter.php
+++ src/Zoop/User/DataModel/AbstractUserFilter.php
@@ -3,10 +3,10 @@
 namespace Zoop\User\DataModel;
 
 use Zoop\User\DataModel\AbstractUser;
-use Zoop\Entity\DataModel\EntityFilterInterface;
+use Zoop\Entity\DataModel\EntitiesFilterInterface;
 use Zoop\Entity\DataModel\EntitiesTrait;
 
-class AbstractUserFilter extends AbstractUser implements EntityFilterInterface
+class AbstractUserFilter extends AbstractUser implements EntitiesFilterInterface
 {
     use EntitiesTrait;
 }
