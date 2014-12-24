23d75339ff57539b89ca48cd33e5f725a5de9314
crimsonronin <josh.stuart@zoopcommerce.com>
2014-12-24 17:29:41 +1100

    Updated dependencies.

--- src/Zoop/User/DataModel/AbstractUserFilter.php
+++ src/Zoop/User/DataModel/AbstractUserFilter.php
@@ -6,6 +6,10 @@ use Zoop\User\DataModel\AbstractUser;
 use Zoop\Entity\DataModel\EntitiesFilterInterface;
 use Zoop\Entity\DataModel\EntitiesTrait;
 
+//Annotation imports
+use Doctrine\ODM\MongoDB\Mapping\Annotations as ODM;
+use Zoop\Shard\Annotation\Annotations as Shard;
+
 class AbstractUserFilter extends AbstractUser implements EntitiesFilterInterface
 {
     use EntitiesTrait;
